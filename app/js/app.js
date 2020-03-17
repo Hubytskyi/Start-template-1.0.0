b669882c60a4d90fc70cadf2a8f0e689f7305cd6
hubytskyi <m.hubytskyi@gmail.com>
2020-03-17 15:59:33 +0200

    change home page

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	const circle = document.querySelectorAll(".svg-circle");
 	circle.forEach( (el) => {
 		window.addEventListener("scroll", function() {
-			el.style.transform = "rotate("+window.pageYOffset+"deg)";
+			el.style = `transform:rotate(${window.pageYOffset}deg)`;
 			});
 	})
 
