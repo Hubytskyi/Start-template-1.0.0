693fb8b0f2abd374f32064039f109945f5fe2299
hubytskyi <m.hubytskyi@gmail.com>
2020-03-20 13:55:03 +0200

    change home, add about and podcast

--- app/js/app.js
+++ app/js/app.js
@@ -7,5 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 			});
 	})
 
+	
+	const aboutFontSize = document.querySelector('.bio__about span')
+	const widthContainer = document.querySelector('.container').clientWidth
+	aboutFontSize.style=`font-size: ${widthContainer / 8.8}px; line-height: ${widthContainer / 10.7}px`;
+
+
+	// const podcastTitle = document.querySelector('.podcast__title')
+	// const podcastTitleItem = podcastTitle.firstElementChild
+	// podcastTitleItem.innerHTML = `${podcastTitleItem.innerHTML} +`
 
 });
