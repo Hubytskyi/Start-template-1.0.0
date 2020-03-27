95e0cde0ade2066943792ec1956a6909fb86258b
hubytskyi <m.hubytskyi@gmail.com>
2020-03-27 17:16:25 +0200

    add services, terms, policy and sub menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	const circle = document.querySelectorAll(".svg-circle");
-	circle.forEach( (el) => {
-		window.addEventListener('scroll', function() {
-			el.style = `transform:rotate(${window.pageYOffset / 2}deg)`;
-			});
-	})
-	
-	// const bioHeader = document.querySelector('.bio__header')
-	// const widthContainer = document.querySelector('.container').clientWidth
-	// bioHeader.style=`font-size: ${widthContainer / 7.7}px; line-height: ${widthContainer / 9.3}px`;
-	
 	var swiper = new Swiper('.swiper-container', {
 		loop: true,
 		pagination: {
@@ -21,6 +10,19 @@ document.addEventListener("DOMContentLoaded", function() {
 			prevEl: '.swiper-button-prev',
 		},
 	});
+
+	const circle = document.querySelectorAll(".svg-circle");
+	circle.forEach( (el) => {
+		window.addEventListener('scroll', function() {
+			el.style = `transform:rotate(${window.pageYOffset / 2}deg)`;
+			});
+	})
+	
+	const bioHeader = document.querySelector('.bio__header')
+	const widthContainer = document.querySelector('.container').clientWidth
+	bioHeader.style=`font-size: ${widthContainer / 7.7}px; line-height: ${widthContainer / 9.3}px`;
+	
+
 
 	// const menuLink = document.querySelectorAll('.menu__link')
 	// menuLink.classList.remove('menu__link--active')
