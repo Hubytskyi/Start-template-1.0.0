0f245d17494c88996844b4616254bb7ab7ed9fef
hubytskyi <m.hubytskyi@gmail.com>
2020-03-27 10:53:26 +0200

    add media items page

--- app/js/app.js
+++ app/js/app.js
@@ -2,16 +2,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	const circle = document.querySelectorAll(".svg-circle");
 	circle.forEach( (el) => {
-		window.addEventListener("scroll", function() {
+		window.addEventListener('scroll', function() {
 			el.style = `transform:rotate(${window.pageYOffset / 2}deg)`;
 			});
 	})
-
 	
 	const bioHeader = document.querySelector('.bio__header')
 	const widthContainer = document.querySelector('.container').clientWidth
 	bioHeader.style=`font-size: ${widthContainer / 7.7}px; line-height: ${widthContainer / 9.3}px`;
 
+	// const menuLink = document.querySelectorAll('.menu__link')
+	// menuLink.classList.remove('menu__link--active')
+	// const path = location.pathname
+	// menuLink.forEach( function() {
+	// 	const href = this.attr('href')
+	// 	if(path.slice(1).substring(0, href.length) === href){
+	// 		menuLink.classList.add('menu__link--active')
+	// 	}
+	// })
+
+	// const menuLink = document.querySelectorAll('.menu__link')
+	// menuLink.forEach( (el) => {
+	// 	menuLink.addEventListener('click', () => {
+	// 		el.classList.add('menu__link--active')
+	// 	})
+	// })
 
 	// const podcastTitle = document.querySelector('.podcast__title')
 	// const podcastTitleItem = podcastTitle.firstElementChild
